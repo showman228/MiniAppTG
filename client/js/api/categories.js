80d69723eefbc27aed2021875565b4ed552ea70c
showman228 <nekrasovaleksey.1999@gmail.com>
2026-04-25 20:48:31 +0300

    add vue.js

--- client/js/api/categories.js
+++
@@ -1,9 +0,0 @@
-import { get } from './client.js';
-
-export async function getCategories() {
-    return get("/category/");
-}
-
-export async function getCategoryById(categoryId) {
-    return get(`/category/${categoryId}`);
-}
