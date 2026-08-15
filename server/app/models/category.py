018c8b8795a9cc98f5d3f5bc47d803e3b27e4b17
showman228 <nekrasovaleksey.1999@gmail.com>
2026-08-15 15:03:01 +0500

    сделал свою первую правильную миграцию и поставил ее

--- server/app/models/category.py
+++ server/app/models/category.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from server.app.database import Base
 
 
@@ -8,6 +8,7 @@ class Category(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, nullable=False, index=True)
     slug = Column(String, unique=True, index=True)
+    action = Column(Boolean)
 
     def __repr__(self):
         return f"<Category id: {self.id}, name: {self.name}, slug: {self.slug}>"
